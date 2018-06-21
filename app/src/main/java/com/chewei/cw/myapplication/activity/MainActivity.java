818b256b676346eb8b8c29e3c1e92c5c9e674350
chewei <weiweichelaixi@163.com>
2018-06-21 17:42:11 +0800

    bug修复

--- app/src/main/java/com/chewei/cw/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/chewei/cw/myapplication/activity/MainActivity.java
@@ -1,9 +1,17 @@
 package com.chewei.cw.myapplication.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.chewei.cw.myapplication.R;
@@ -21,12 +29,37 @@ public class MainActivity extends BaseActivity {
     ArrayList<ItemBean> list;
     @BindView(R.id.recycle_view)
     RecyclerView recycleView;
+    @BindView(R.id.iv_more)
+    ImageView ivMore;
+    @BindView(R.id.iv_search_icon)
+    ImageView ivSearchIcon;
+    @BindView(R.id.textview_search)
+    TextView textviewSearch;
+    @BindView(R.id.rl_search)
+    LinearLayout rlSearch;
+    @BindView(R.id.iv_more_icon)
+    ImageView ivMoreIcon;
+    @BindView(R.id.iv_search_iv)
+    ImageView ivSearchIv;
+    @BindView(R.id.textview_search_tv)
+    TextView textviewSearchTv;
+    @BindView(R.id.rl_search_layout)
+    LinearLayout rlSearchLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.toolbar1)
+    public View mToolbar1;
+    @BindView(R.id.toolbar2)
+    public View mToolbar2;
+    @BindView(R.id.toolbar_layout)
+    CollapsingToolbarLayout toolbarLayout;
+    @BindView(R.id.app_bar)
+    AppBarLayout appBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recycleView = $(R.id.recycle_view);
         ButterKnife.bind(this);
         inintData();
         GridLayoutManager layoutManager = new GridLayoutManager(this, 5);
@@ -56,6 +89,54 @@ public class MainActivity extends BaseActivity {
                 }
             }
         });
+        appBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (verticalOffset == 0) {
+                    //张开
+                    mToolbar1.setVisibility(View.VISIBLE);
+                    mToolbar2.setVisibility(View.GONE);
+                    setToolbar1Alpha(255);
+                } else if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
+                    //收缩
+                    mToolbar1.setVisibility(View.GONE);
+                    mToolbar2.setVisibility(View.VISIBLE);
+                    setToolbar2Alpha(255);
+                } else {
+                    int alpha = 255 - Math.abs(verticalOffset);
+                    if (alpha < 0) {
+                        //收缩toolbar
+                        mToolbar1.setVisibility(View.GONE);
+                        mToolbar2.setVisibility(View.VISIBLE);
+                        setToolbar2Alpha(Math.abs(verticalOffset));
+                    } else {
+                        //张开toolbar
+                        mToolbar1.setVisibility(View.VISIBLE);
+                        mToolbar2.setVisibility(View.GONE);
+                        setToolbar1Alpha(alpha);
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * 设置展开时各控件的透明度
+     */
+    public void setToolbar1Alpha(int alpha) {
+        rlSearch.getBackground().setAlpha(alpha);
+        ivSearchIcon.getDrawable().setAlpha(alpha);
+        textviewSearch.setTextColor(Color.argb(alpha, 255, 255, 255));
+        ivMore.getDrawable().setAlpha(alpha);
+    }
+
+    /**
+     * 设置闭合时各控件的透明度
+     */
+    public void setToolbar2Alpha(int alpha) {
+        ivMoreIcon.getDrawable().setAlpha(alpha);
+        ivSearchIv.getDrawable().setAlpha(alpha);
+        rlSearchLayout.getBackground().setAlpha(alpha);
     }
 
     private void inintData() {

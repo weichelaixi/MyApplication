0a6d585719a6751ff7c86f1184f27fc5e3b3304c
chewei <weiweichelaixi@163.com>
2018-07-19 18:35:44 +0800

    fragment

---
+++ module_fragmentation/src/main/java/com/chewei/module_fragmentation/LifeCycleActivity.java
@@ -0,0 +1,106 @@
+package com.chewei.module_fragmentation;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+
+import com.chewei.module_fragmentation.fragments.FragmentOne;
+import com.chewei.module_fragmentation.fragments.FragmentThree;
+import com.chewei.module_fragmentation.fragments.FragmentTwo;
+import com.weiche.module_common.BaseActivity;
+import com.weiche.module_common.utils.KeepLog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ${chewei} on 2018/7/12.
+ * params:2018/7/12
+ */
+
+public class LifeCycleActivity extends BaseActivity {
+    List<Fragment> fragments;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        KeepLog.e("   Activity-->onCreate");
+        setContentView(R.layout.lifecycle_activity_layout);
+        Toolbar toolbar = $(R.id.toolbar);
+        setupToolBar(toolbar,false);
+        inintView();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        KeepLog.e("   Activity-->onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        KeepLog.e("   Activity-->onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        KeepLog.e("   Activity-->onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        KeepLog.e("   Activity-->onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        KeepLog.e("   Activity-->onDestroy");
+    }
+
+    private void inintView() {
+        TabLayout tab_main_activity = $(R.id.tab_main_activity);
+        ViewPager vp_main_activity = $(R.id.vp_main_activity);
+        fragments = new ArrayList<>();
+        fragments.add(new FragmentOne());
+        fragments.add(new FragmentTwo());
+        fragments.add(new FragmentThree());
+        vp_main_activity.setOffscreenPageLimit(2);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(),fragments);
+        vp_main_activity.setAdapter(adapter);
+        tab_main_activity.setupWithViewPager(vp_main_activity);
+        for (int i = 0; i < adapter.getCount(); i++) {
+            tab_main_activity.getTabAt(i).setText("tab"+(i+1));
+        }
+    }
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        List<Fragment> fragments;
+        FragmentManager fm;
+
+        public ViewPagerAdapter(FragmentManager fm,List<Fragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+            this.fm = fm;
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+    }
+}
---
+++ module_fragmentation/src/main/java/com/chewei/module_fragmentation/fragments/FragmentOne.java
@@ -0,0 +1,90 @@
+package com.chewei.module_fragmentation.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.chewei.module_fragmentation.R;
+import com.weiche.module_common.utils.KeepLog;
+
+/**
+ * Created by ${chewei} on 2018/7/12.
+ * params:2018/7/12
+ */
+
+public class FragmentOne extends Fragment {
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        KeepLog.e("   FragmentOne-->onAttach");
+    }
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        KeepLog.e("    FragmentOne-->onCreate");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+        KeepLog.e("    FragmentOne-->onCreateView");
+        View view = inflater.inflate(R.layout.fragment_layout,container,false);
+        TextView textView = view.findViewById(R.id.content_tv);
+        textView.setText("one");
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated( Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        KeepLog.e("   FragmentOne-->onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        KeepLog.e("   FragmentOne-->onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        KeepLog.e("   FragmentOne-->onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        KeepLog.e("   FragmentOne-->onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        KeepLog.e("   FragmentOne-->onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        KeepLog.e("   FragmentOne-->onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        KeepLog.e("   FragmentOne-->onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        KeepLog.e("   FragmentOne-->onDetach");
+    }
+}

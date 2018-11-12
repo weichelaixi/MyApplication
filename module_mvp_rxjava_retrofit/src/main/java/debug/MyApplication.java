4538542852837bfa0520e23bfa99ab77c032924b
chewei <1012701589@qq.com>
2018-11-12 23:23:35 +0800

    。。。

--- module_mvp_rxjava_retrofit/src/main/java/debug/MyApplication.java
+++ module_mvp_rxjava_retrofit/src/main/java/debug/MyApplication.java
@@ -1,8 +1,15 @@
 package debug;
 
 import android.content.Context;
+import android.support.annotation.Keep;
+import android.util.Log;
 
 import com.chewei.module_mvp.util.AppContextUtil;
+import com.taobao.sophix.PatchStatus;
+import com.taobao.sophix.SophixApplication;
+import com.taobao.sophix.SophixEntry;
+import com.taobao.sophix.SophixManager;
+import com.taobao.sophix.listener.PatchLoadStatusListener;
 import com.weiche.module_common.BaseApplication;
 
 /**
@@ -10,10 +17,15 @@ import com.weiche.module_common.BaseApplication;
  * params:2018/11/6
  */
 
-public class MyApplication extends BaseApplication {
+public class MyApplication extends SophixApplication {
 
+    private final String TAG = "MyApplication";
     public static Context applicationContext;
 
+    // 此处SophixEntry应指定真正的Application，并且保证RealApplicationStub类名不被混淆。
+    @Keep
+    @SophixEntry(MyApplication.class)
+    static class RealApplicationStub {}
 
     public static Context getContext() {
         return applicationContext;
@@ -25,4 +37,40 @@ public class MyApplication extends BaseApplication {
         AppContextUtil.init(this);
         applicationContext = this;
     }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+//         如果需要使用MultiDex，需要在此处调用。
+//         MultiDex.install(this);
+        initSophix();
+    }
+
+    private void initSophix() {
+        String appVersion = "0.0.0";
+        try {
+            appVersion = this.getPackageManager()
+                    .getPackageInfo(this.getPackageName(), 0)
+                    .versionName;
+        } catch (Exception e) {
+        }
+        final SophixManager instance = SophixManager.getInstance();
+        instance.setContext(this)
+                .setAppVersion(appVersion)
+                .setSecretMetaData(null, null, null)
+                .setEnableDebug(true)
+                .setEnableFullLog()
+                .setPatchLoadStatusStub(new PatchLoadStatusListener() {
+                    @Override
+                    public void onLoad(final int mode, final int code, final String info, final int handlePatchVersion) {
+                        if (code == PatchStatus.CODE_LOAD_SUCCESS) {
+                            Log.i(TAG, "sophix load patch success!");
+                        } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
+                            // 如果需要在后台重启，建议此处用SharePreference保存状态。
+                            Log.i(TAG, "sophix preload patch success. restart app to make effect.");
+                        }
+                    }
+                }).initialize();
+    }
+
 }
